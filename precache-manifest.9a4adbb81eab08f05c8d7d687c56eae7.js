a4a7fb697bdb334a0673288b3c1b3197a4b102d5
vaso369 <vasilije.vasilijevic.11.17@ict.edu.rs>
2020-10-21 18:34:25 +0200

    Updates

---
+++ precache-manifest.9a4adbb81eab08f05c8d7d687c56eae7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b32621022b4135769fad77150075dee",
+    "url": "/admin-dashboard/index.html"
+  },
+  {
+    "revision": "560a9f80c41238c01545",
+    "url": "/admin-dashboard/static/css/2.267f6ae4.chunk.css"
+  },
+  {
+    "revision": "7b03a8591e9a76a9f9ab",
+    "url": "/admin-dashboard/static/css/main.3c8d74d1.chunk.css"
+  },
+  {
+    "revision": "560a9f80c41238c01545",
+    "url": "/admin-dashboard/static/js/2.41b1c725.chunk.js"
+  },
+  {
+    "revision": "736f3ad4ee5990319c2a87119f7ebea6",
+    "url": "/admin-dashboard/static/js/2.41b1c725.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b03a8591e9a76a9f9ab",
+    "url": "/admin-dashboard/static/js/main.7cf133f1.chunk.js"
+  },
+  {
+    "revision": "e935f5d8a468fe457fde",
+    "url": "/admin-dashboard/static/js/runtime-main.e3032649.js"
+  },
+  {
+    "revision": "212c5f9fccf791f0640d139253ff2785",
+    "url": "/admin-dashboard/static/media/design.212c5f9f.png"
+  },
+  {
+    "revision": "ca2951b9d04f0e7f55c6b3ad7a2e6a80",
+    "url": "/admin-dashboard/static/media/loginBackground.ca2951b9.svg"
+  },
+  {
+    "revision": "058810c9de25accdf9a193c67b1e6a75",
+    "url": "/admin-dashboard/static/media/more-news.058810c9.svg"
+  },
+  {
+    "revision": "39f1b26f29318a6337e9e40f75ff0284",
+    "url": "/admin-dashboard/static/media/project.39f1b26f.svg"
+  },
+  {
+    "revision": "4699a34fb964db2ff2236e3a36f34904",
+    "url": "/admin-dashboard/static/media/search.4699a34f.svg"
+  }
+]);
